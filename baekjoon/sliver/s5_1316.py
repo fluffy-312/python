3447f0183fa39d7095104444883ed0e634f0eec3
fluffy-312 <donghyun.kwag@gmal.com>
2025-12-04 22:18:42 +0900

    solve 1316

--- baekjoon/sliver/s5_1316.py
+++ baekjoon/sliver/s5_1316.py
@@ -1,7 +1,17 @@
 N = int(input())
+set1 = set()
 
 for i in range(N):
-    w = input()
-    set1 = set([w[i] for i in range(len(w))])
-    if len(set1) != len(w):
-        pass
+    w = list(input())
+    set1.update(*w)
+    try:
+        for j in range(len(w)):
+            if w[j] != w[j+1]:
+                w[j] = "!"
+    except IndexError:
+        w[j] = "!"
+    if w.count("!") != len(set1):
+        N -= 1
+    set1.clear()
+
+print(N)
